47e4cf8d396fe20757dfdf246150bb29fd08dae8
Сергей <suvalov87@gmail.com>
2021-08-25 14:44:26 +0500

    задание 5 выполнено!

--- 04 lesson/05_snowfall.py
+++ 04 lesson/05_snowfall.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
+sd.resolution = 1200, 600
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
@@ -17,12 +19,81 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+# sd.snowflake(center=point_0, length=200, factor_a=0.5)
+list_x = [50,300,500,250,280,400,300,350,500,700,800,1000]
+list_y = [500,490,480,485,490,495,500,495,470,480,475,485,490,465,460]
+list_length = [random.randrange(1,50,5), random.randrange(1,50,5), random.randrange(1,50,5), random.randrange(1,50,5),
+               random.randrange(1,50,5),random.randrange(1,50,5),random.randrange(1,50,5),random.randrange(1,50,5)]
+shift_x = random.randrange(0, 50, 1)
+shift_x1 = random.randrange(0, 50, 1)
+shift_x2 = random.randrange(0, 50, 1)
+shift_x3 = random.randrange(0, 50, 1)
+shift_x4 = random.randrange(0, 50, 1)
+shift_x5 = random.randrange(0, 50, 1)
 while True:
     sd.clear_screen()
-    pass
-    pass
-    pass
+    point_0 = sd.get_point(list_x[0], list_y[0])
+    sd.snowflake(center=point_0, length=list_length[0])
+    list_y[0] -= 10
+    if list_y[0] < 20:
+        break
+    list_x[0] = list_x[0] + shift_x
+
+    point_1 = sd.get_point(list_x[1], list_y[1])
+    sd.snowflake(center=point_1, length=list_length[1])
+    list_y[1] -= 10
+    if list_y[1] < 20:
+        break
+    list_x[1] = list_x[1] + shift_x1
+
+    point_2 = sd.get_point(list_x[2], list_y[2])
+    sd.snowflake(center=point_2, length=list_length[2])
+    list_y[2] -= 10
+    if list_y[2] < 20:
+        break
+    list_x[2] = list_x[2] + shift_x2
+
+    point_3 = sd.get_point(list_x[3], list_y[3])
+    sd.snowflake(center=point_3, length=list_length[3])
+    list_y[3] -= 10
+    if list_y[3] < 20:
+        break
+    list_x[3] = list_x[3] + shift_x3
+
+    point_4 = sd.get_point(list_x[4], list_y[4])
+    sd.snowflake(center=point_4, length=list_length[4])
+    list_y[4] -= 10
+    if list_y[4] < 20:
+        break
+    list_x[4] = list_x[4] + shift_x4
+
+    point_5 = sd.get_point(list_x[5], list_y[5])
+    sd.snowflake(center=point_5, length=list_length[5])
+    list_y[5] -= 10
+    if list_y[5] < 20:
+        break
+    list_x[5] = list_x[5] + shift_x5
+
+    point_6 = sd.get_point(list_x[6], list_y[6])
+    sd.snowflake(center=point_6, length=list_length[6])
+    list_y[6] -= 10
+    if list_y[6] < 20:
+        break
+    list_x[6] = list_x[6] + shift_x1
+
+    point_7 = sd.get_point(list_x[7], list_y[7])
+    sd.snowflake(center=point_7, length=list_length[7])
+    list_y[7] -= 10
+    if list_y[7] < 20:
+        break
+    list_x[7] = list_x[7] + shift_x2
+
+    point_8 = sd.get_point(list_x[8], list_y[8])
+    sd.snowflake(center=point_8, length=list_length[2])
+    list_y[8] -= 10
+    if list_y[8] < 20:
+        break
+    list_x[8] = list_x[8] + shift_x3
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
