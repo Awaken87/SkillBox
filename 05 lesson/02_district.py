caea4f353fb68457cc48cd703b1492ea91da73b5
Сергей <suvalov87@gmail.com>
2021-08-26 18:42:25 +0500

    Задание №2, 5 урока выполнено

--- 05 lesson/02_district.py
+++ 05 lesson/02_district.py
@@ -5,8 +5,17 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as folks_1
+from district.central_street.house1.room2 import folks as folks_2
+from district.central_street.house2.room1 import folks as folks_3
+from district.central_street.house2.room2 import folks as folks_4
+from district.soviet_street.house1.room1 import folks as folks_5
+from district.soviet_street.house1.room2 import folks as folks_6
+from district.soviet_street.house2.room1 import folks as folks_7
+from district.soviet_street.house2.room2 import folks as folks_8
 
-
+s = ', '
+print('На районе живут', s.join(folks_1), s.join(folks_2), s.join(folks_3), s.join(folks_4), s.join(folks_5),
+      s.join(folks_6), s.join(folks_7), s.join(folks_8))
 
 
