43b7cf891da82dcc90bb3d1955070dcd2743d3e6
Сергей <suvalov87@gmail.com>
2021-08-26 18:16:12 +0500

    Задание №1, 5 урока выполнено

--- 05 lesson/01_inhabitants.py
+++ 05 lesson/01_inhabitants.py
@@ -3,6 +3,13 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as folks_1
+from district.central_street.house1.room2 import folks as folks_2
+print('В комнате room_1 живут:', folks_1)
+print('В комнате room_2 живут:', folks_2)
+
+
+
+
 
 
